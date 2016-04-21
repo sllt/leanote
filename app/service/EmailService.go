a9313ce7d63fcd467d1fc433404fc21ca6beed18
sllt <shenlongml@gmail.com>
2016-04-21 20:18:49 +0800

    add smtp tls support

--- app/service/EmailService.go
+++ app/service/EmailService.go
@@ -2,12 +2,15 @@ package service
 
 import (
 	"bytes"
+	"crypto/tls"
 	"fmt"
 	"github.com/leanote/leanote/app/db"
 	"github.com/leanote/leanote/app/info"
 	. "github.com/leanote/leanote/app/lea"
+	"github.com/revel/revel"
 	"gopkg.in/mgo.v2/bson"
 	"html/template"
+	"net"
 	"net/smtp"
 	"strconv"
 	"strings"
@@ -37,6 +40,64 @@ func InitEmailFromDb() {
 	password = configService.GetGlobalStringConfig("emailPassword")
 }
 
+func Dial(addr string) (*smtp.Client, error) {
+	conn, err := tls.Dial("tcp", addr, nil)
+	if err != nil {
+		fmt.Println("Dialing Error:", err)
+		return nil, err
+	}
+	host, _, _ := net.SplitHostPort(addr)
+	return smtp.NewClient(conn, host)
+}
+
+func SendEMailUsingTLS(addr string, auth smtp.Auth, from string,
+	to []string, msg []byte) (err error) {
+
+	//create smtp client
+	c, err := Dial(addr)
+	if err != nil {
+		fmt.Println("Create smtp client error:", err)
+		return err
+	}
+	defer c.Close()
+
+	if auth != nil {
+		if ok, _ := c.Extension("AUTH"); ok {
+			if err = c.Auth(auth); err != nil {
+				fmt.Println("Error during AUTH", err)
+				return err
+			}
+		}
+	}
+
+	if err = c.Mail(from); err != nil {
+		return err
+	}
+
+	for _, addr := range to {
+		if err = c.Rcpt(addr); err != nil {
+			return err
+		}
+	}
+
+	w, err := c.Data()
+	if err != nil {
+		return err
+	}
+
+	_, err = w.Write(msg)
+	if err != nil {
+		return err
+	}
+
+	err = w.Close()
+	if err != nil {
+		return err
+	}
+
+	return c.Quit()
+}
+
 func (this *EmailService) SendEmail(to, subject, body string) (ok bool, e string) {
 	InitEmailFromDb()
 
@@ -57,6 +118,15 @@ func (this *EmailService) SendEmail(to, subject, body string) (ok bool, e string
 
 	msg := []byte("To: " + to + "\r\nFrom: " + username + "<" + username + ">\r\nSubject: " + subject + "\r\n" + content_type + "\r\n\r\n" + body)
 	send_to := strings.Split(to, ";")
+
+	usetls, _ := revel.Config.Bool("email.stmp.tls.enable")
+
+	if usetls == true {
+		fmt.Println("use tls")
+		err := SendEMailUsingTLS(host+":"+emailPort, auth, username, send_to, msg)
+		fmt.Println(err)
+		return
+	}
 	err := smtp.SendMail(host+":"+emailPort, auth, username, send_to, msg)
 
 	if err != nil {
